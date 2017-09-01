18ac91a06dbbea3e212dd58894bac4c385b4076a
AlexKott <alexey.kott@gmail.com>
2017-09-02 00:57:38 +0300

    first commit

---
+++ bot.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+import telebot
+from telebot import types
+import config as cfg 
+
+
+bot = telebot.TeleBot(cfg.token)
+
+pin_user = [59863436]
+
+
+@bot.message_handler(content_types = ['text'])
+def reply(message):
+	if message.from_user.id in pin_user:
+		bot.pin_chat_message(message.chat.id, message.message_id)
+
+if __name__ == '__main__':
+	bot.polling(none_stop=True)
+
